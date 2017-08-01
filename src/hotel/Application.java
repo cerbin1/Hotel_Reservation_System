b13f4ce8cba086150a4254796b4467fabb7d5a2c
Bartek <bartek9393@poczta.onet.pl>
2017-08-01 22:36:22 +0200

    Remvoed using SSL while connecting to database

--- src/hotel/Application.java
+++ src/hotel/Application.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 public class Application {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/hotel";
+        String url = "jdbc:mysql://localhost:3306/hotel?useSSL=false";
         String login = "root";
         String password = "root";
         try {
